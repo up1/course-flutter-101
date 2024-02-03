7981a3aa22ae34a411a4d3b8330baa92eebc963d
up1 <somkiat.p@gmail.com>
2024-02-03 15:35:21 +0700

    Create product service

---
+++ demo/day02/lib/services/product_service.dart
@@ -0,0 +1,31 @@
+import 'package:day02/models/product_response.dart';
+import 'package:dio/dio.dart';
+
+class ProductService {
+  Dio _dio = Dio();
+
+  Future<ProductResponse> fetchAllProduct(int limit) async {
+    try {
+      BaseOptions options = BaseOptions(
+        baseUrl: "https://dummyjson.com",
+        receiveDataWhenStatusError: true,
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+      );
+      _dio = Dio(options);
+      final response = await _dio.get('/products?limit=$limit');
+
+      if (response.statusCode == 200) {
+        return ProductResponse.fromJson(response.data);
+      } else if (response.statusCode == 400) {
+        throw Exception('Bad request');
+      } else if (response.statusCode == 500) {
+        throw Exception('Server error');
+      } else {
+        throw Exception('Unexpected error occurred');
+      }
+    } catch (e) {
+      throw Exception('Failed to login: $e');
+    }
+  }
+}
